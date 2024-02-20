52ad0031f6c8023bbd75dd699a76f03e339bd6d0
arcsagar <128665023+arcsagar@users.noreply.github.com>
2024-02-20 22:01:18 +0530

    Update MainComponent.tsx
    from sagar

--- src/component/MainComponent/MainComponent.tsx
+++ src/component/MainComponent/MainComponent.tsx
@@ -3,11 +3,29 @@ import React, { useContext, useState } from "react";
 import AuthComponent from "../Auth/AuthComponent";
 import AdminComponent from "../features/Admin/AdminComponent";
 import UserComponent from "../features/User/UserComponent";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import SignInComponent from "../Auth/SignInComponent/SignInComponent";
 
 export const userContext = React.createContext<any>({});
 
 const MainComponent = () =>{
 
+  const route = [
+    {
+      path: '/',
+      element: <SignInComponent />
+    },
+    {
+      path: 'admin',
+      element: <AdminComponent />
+    },
+    {
+      path: 'user',
+      element: <UserComponent />
+    }
+  ];
+
+  
     const [loginUserDetails, setLoginUserDetails] = useState<any>({});
     const [isSignUp, setIsSignUp] = useState(false);
   
@@ -18,13 +36,18 @@ const MainComponent = () =>{
         setLoginUserDetails
       }
   
+      const routes: any = createBrowserRouter(route);
     return (
-        <userContext.Provider value={allObj}>
-          {Object.keys(loginUserDetails).length === 0 && <AuthComponent />}
-          {loginUserDetails.userType === 'user' && <UserComponent /> }
-          {loginUserDetails.userType === 'admin' && <AdminComponent /> }
-        </userContext.Provider>
-   
+      <userContext.Provider value={allObj}>
+            <RouterProvider router={routes} />
+      </userContext.Provider>
+       
+      //   <userContext.Provider value={allObj}>
+      //   {Object.keys(loginUserDetails).length === 0 && <AuthComponent />}
+      //   {loginUserDetails.userType === 'user' && <UserComponent /> }
+      //   {loginUserDetails.userType === 'admin' && <AdminComponent /> }
+      // </userContext.Provider>
+ 
     );
   }
     
