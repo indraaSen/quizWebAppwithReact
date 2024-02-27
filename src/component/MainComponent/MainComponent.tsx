64ff9e89a7a854b210ed6c38070db28c00afd6c1
indraaSen <waghmarenishu8@gmail.com>
2024-02-27 14:14:28 +0530

    quiz app with react check-8

--- src/component/MainComponent/MainComponent.tsx
+++ src/component/MainComponent/MainComponent.tsx
@@ -3,7 +3,6 @@ import React, { useContext, useState } from "react";
 import AuthComponent from "../Auth/AuthComponent";
 import AdminComponent from "../features/Admin/AdminComponent";
 import UserComponent from "../features/User/UserComponent";
-<<<<<<< HEAD
 import { BrowserRouter, Route, RouterProvider, Routes, createBrowserRouter, useNavigate} from "react-router-dom";
 import SignInComponent from "../Auth/SignInComponent/SignInComponent";
 import SignUpComponent from "../Auth/SignUpComponent/SignUpComponent";
@@ -18,31 +17,11 @@ import AllUsers from "../features/Admin/AllUsers/AllUsers";
 import AdminSetting from "../features/Admin/AdminSetting/AdminSetting";
 import ErrorHandle from "../ErrorHandle/ErrorHandle";
 
-=======
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import SignInComponent from "../Auth/SignInComponent/SignInComponent";
->>>>>>> 7421a259abc6e9db83a3f54e51f04ee13a60e38d
 
 export const userContext = React.createContext<any>({});
 
 const MainComponent = () =>{
 
-  const route = [
-    {
-      path: '/',
-      element: <SignInComponent />
-    },
-    {
-      path: 'admin',
-      element: <AdminComponent />
-    },
-    {
-      path: 'user',
-      element: <UserComponent />
-    }
-  ];
-
-  
     const [loginUserDetails, setLoginUserDetails] = useState<any>({});
     const [isSignUp, setIsSignUp] = useState(false);
     const [singUpBtn, setSingUpBtn] = useState([]);
@@ -55,7 +34,6 @@ const MainComponent = () =>{
         singUpBtn,
         setSingUpBtn
       }
-<<<<<<< HEAD
 
     return (
        
@@ -81,24 +59,7 @@ const MainComponent = () =>{
           <Route path="*" element={<ErrorHandle />} />
         </Routes>
       </BrowserRouter>
-        {/* {Object.keys(loginUserDetails).length === 0 } */}
-        {/* {loginUserDetails.userType === 'user' && <UserComponent /> }
-        {loginUserDetails.userType === 'admin' && <AdminComponent /> } */}
       </userContext.Provider>
-=======
-  
-      const routes: any = createBrowserRouter(route);
-    return (
-      <userContext.Provider value={allObj}>
-            <RouterProvider router={routes} />
-      </userContext.Provider>
-       
-      //   <userContext.Provider value={allObj}>
-      //   {Object.keys(loginUserDetails).length === 0 && <AuthComponent />}
-      //   {loginUserDetails.userType === 'user' && <UserComponent /> }
-      //   {loginUserDetails.userType === 'admin' && <AdminComponent /> }
-      // </userContext.Provider>
->>>>>>> 7421a259abc6e9db83a3f54e51f04ee13a60e38d
  
     );
   }
